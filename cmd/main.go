3561fc052989a949e5831276221ec2e0fc95424d
mykhailo.prokopchyk <mykhailo.prokopchyk@yalantis.net>
2022-03-22 18:54:46 +0200

    add option to read brainfuck programs from file

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"flag"
+	"github.com/mishaprokop4ik/brainfuck/internal/input"
 	"github.com/mishaprokop4ik/brainfuck/internal/interpreter"
+	"log"
 )
 
+var programPath = flag.String("f", "", "brainfuck program file path")
 const defaultDataIn string = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.\n"
 
 func main()  {
-	interpreter.Interpret(defaultDataIn)
+	flag.Parse()
+	fromFile := true
+	if *programPath == "" {
+		*programPath = defaultDataIn
+		fromFile = false
+	}
+	dataIn, err := input.NewFile(*programPath,  fromFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	interpreter.Interpret(dataIn.OutText())
 }
