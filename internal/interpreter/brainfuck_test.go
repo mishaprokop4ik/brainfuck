9a20115cdf71820e3d39540c70719fd168a60326
mykhailo.prokopchyk <mykhailo.prokopchyk@yalantis.net>
2022-03-24 22:45:51 +0200

    add test of unexpected symbols

--- internal/interpreter/brainfuck_test.go
+++ internal/interpreter/brainfuck_test.go
@@ -13,10 +13,15 @@ func TestInterpret(t *testing.T) {
 			"hello world program",
 			"-[------->+<]>-.-[->+++++<]>++.+++++++..+++.[--->+<]>-----.--[->++++<]>-.--------.+++.------.--------.-[--->+<]>.",
 		},
+		{
+			"unexpected symbol",
+			"p",
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			defer func() { recover() }()
 			// nolint scopelint
 			Interpret(tt.args)
 		})
